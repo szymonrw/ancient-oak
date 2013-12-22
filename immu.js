e169d386cb35d2354a12e8606ab957f6dffd1d3a
Szymon Witamborski <szywon@szywon.pl>
2013-12-22 18:36:23 +0000

    Unlimited history.
    Soon to be limited.

--- immu.js
+++ immu.js
@@ -34,29 +34,31 @@ function is_api (value) {
           typeof value.patch === "function");
 }
 
-function inherit (storage) {
-  function Inheriting () {}
-  Inheriting.prototype = storage;
-  return new Inheriting;
-}
-
-function merge (storage1, storage2) {
-  return store_object(inherit(storage1), storage2);
-}
-
-function api (storage, deleted) {
+function api (storage, deleted, previous) {
   deleted = deleted || {};
 
-  function iface (name) {
-    return deleted[name] ? null : storage[name];
+  if (!previous) {
+    previous = function prev () { return null; };
+    previous.keys = function keys () { return []; };
+    previous.dump = function dump () { return {}; };
+    previous.back = function back () { return previous; };
   }
 
-  iface.patch = patch;
-  iface.rm = rm;
-  iface.keys = keys;
-  iface.dump = dump;
+  function get (name) {
+    return (deleted[name]
+            ? null
+            : (name in storage
+               ? storage[name]
+               : previous(name)));
+  }
 
-  return iface;
+  get.patch = patch;
+  get.rm = rm;
+  get.keys = keys;
+  get.dump = dump;
+  get.back = back;
+
+  return get;
 
   function patch (diff_in) {
     // diff_in can be a tree of updated values to to patch on top of
@@ -68,46 +70,49 @@ function api (storage, deleted) {
     }
 
     var storage_diff = {};
-    var deleted_diff = {};
 
     var name, old, value;
     for (name in diff_in) {
-      old = storage[name];
+      old = get(name);
       value = diff_in[name];
 
-      deleted_diff[name] = false;
       storage_diff[name] = (!is_immutable(value) && is_api(old)
                             ? old.patch(value)
-                            : value);
+                            : store(value));
     }
 
-    return update(storage_diff, deleted_diff);
+    return update(storage_diff, {});
   }
 
   function rm (name) {
-    if (arguments.length > 1 && is_api(storage[name])) {
-      return patch(name, storage[name].rm.apply(null, slice(arguments, 1)));
+    if (arguments.length > 1) {
+      var element = get(name);
+      console.log(element, is_api(element));
+      if (is_api(element)) {
+        return patch(name, element.rm.apply(null, slice(arguments, 1)));
+      }
     }
 
-    var storage_diff = {};
     var deleted_diff = {};
 
-    storage_diff[name] = undefined;
     deleted_diff[name] = true;
 
-    return update(storage_diff, deleted_diff);
+    return update({}, deleted_diff);
   }
 
   function update (storage_diff, deleted_diff) {
-    return api(merge(storage, storage_diff),
-               merge(deleted, deleted_diff));
+    return api(storage_diff, deleted_diff, get);
   }
 
   function keys () {
-    var ks = [];
+    var ks = previous.keys().filter(function (name) {
+      return !deleted[name];
+    });
+
     for (var name in storage) {
-      if (!deleted[name]) ks.push(name);
+      ks.push(name);
     }
+
     return ks;
   }
 
@@ -115,18 +120,22 @@ function api (storage, deleted) {
     var value = {};
 
     keys().forEach(function (name) {
-      var element = storage[name];
-      value[name] = element && typeof element.dump === "function" ? element.dump() : element;
+      var element = get(name);
+      value[name] = is_api(element) ? element.dump() : element;
     });
 
     return value;
   }
 
-  // iface.on = on;
-  // iface.map = map;
-  // iface.forEach = forEach;
-  // iface.filter = filter;
-  // iface.reduce = reduce;
+  function back () {
+    return previous;
+  }
+
+  // get.on = on;
+  // get.map = map;
+  // get.forEach = forEach;
+  // get.filter = filter;
+  // get.reduce = reduce;
 }
 
 var v1 = store({a: 1, b: 2, c: 3});
@@ -138,7 +147,9 @@ var v5 = store({a: {b: 2, c: 3}, d: {e: 5, f: 7}});
 var v6 = v5.patch({a: {b: 8, g: {h: 9}}});
 var v7 = v6.rm("a", "g", "h");
 
+var assert = require("assert");
 
+console.log(v1.keys(), v2.back().keys());
 console.log("1", v1.dump());
 console.log("2", v2.dump());
 console.log("3", v3.dump());
@@ -146,3 +157,11 @@ console.log("4", v4.dump());
 console.log("5", v5.dump());
 console.log("6", v6.dump());
 console.log("7", v7.dump());
+
+assert.deepEqual(v1.dump(), { a: 1, b: 2, c: 3 });
+assert.deepEqual(v2.dump(), { b: 4, d: { '0': 1, '1': 5 }, a: 1, c: 3 });
+assert.deepEqual(v3.dump(), { h: 8, a: { e: 7 }, b: 4, d: { '0': 1, '1': 5 }, c: 3 });
+assert.deepEqual(v4.dump(), { d: { '1': 5 }, h: 8 });
+assert.deepEqual(v5.dump(), { a: { b: 2, c: 3 }, d: { e: 5, f: 7 } });
+assert.deepEqual(v6.dump(), { a: { b: 8, g: { h: 9 }, c: 3 }, d: { e: 5, f: 7 } });
+assert.deepEqual(v7.dump(), { a: { g: {}, b: 8, c: 3 }, d: { e: 5, f: 7 } });
