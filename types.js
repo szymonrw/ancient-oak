da99a648a18ed97dd7e4613f75c7acea827025ee
Szymon Witamborski <szywon@szywon.pl>
2014-01-05 21:07:50 +0000

    Simple wrapper for hashes.

--- types.js
+++ types.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var array = require("./lib/types/array")(4);
+var hash = require("./lib/types/hash")(2);
 
 function p (a) {
   console.log("--- length: ", a.size);
@@ -8,7 +9,10 @@ function p (a) {
 }
 
 var a = array([6,7,8,9]);
+var b = hash({asdf: 123, qwer: 456});
+
 p(a);
+p(b);
 p(a.map(function (v) { return v + 100 }));
 p(a.set(100, "asdf"));
 console.log("last", a.set(100, "asdf").pop().last());
