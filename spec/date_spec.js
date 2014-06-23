d12140f5ead4f800fef83840dbedf46773430db9
Szymon Witamborski <szywon@szywon.pl>
2014-06-23 19:17:47 +0100

    Get date fields.

--- spec/date_spec.js
+++ spec/date_spec.js
@@ -2,30 +2,87 @@
 
 var store = require("../lib");
 
-describe("store", function () {
+describe("storing dates, ", function () {
 
   describe("stored date", function () {
-    var date = new Date();
-    var result = store(date)();
-
     it("is the same value as original", function () {
-      expect(result).toEqual(date);
-    });
+      var date = new Date();
+      var result = store(date).value;
 
-    it("is not the same object as original", function () {
-      expect(result).not.toBe(date);
+      expect(result).toEqual(date.valueOf());
+    });
+  });
+
+  describe("allows getting date fields", function () {
+    var date = new Date();
+    var stored = store(date);
+
+    var specs = {
+      date: "getDate",
+      Date: "getDate",
+      day: "getDay",
+      Day: "getDay",
+      fullYear: "getFullYear",
+      FullYear: "getFullYear",
+      full_year: "getFullYear",
+      hours: "getHours",
+      Hours: "getHours",
+      milliseconds: "getMilliseconds",
+      Milliseconds: "getMilliseconds",
+      minutes: "getMinutes",
+      Minutes: "getMinutes",
+      month: "getMonth",
+      Month: "getMonth",
+      seconds: "getSeconds",
+      Seconds: "getSeconds",
+      time: "getTime",
+      Time: "getTime",
+      timezoneOffset: "getTimezoneOffset",
+      TimezoneOffset: "getTimezoneOffset",
+      timezone_offset: "getTimezoneOffset",
+      utcDate: "getUTCDate",
+      UTCDate: "getUTCDate",
+      utc_date: "getUTCDate",
+      utcDay: "getUTCDay",
+      UTCDay: "getUTCDay",
+      utc_day: "getUTCDay",
+      utcFullYear: "getUTCFullYear",
+      UTCFullYear: "getUTCFullYear",
+      utc_full_year: "getUTCFullYear",
+      utcHours: "getUTCHours",
+      UTCHours: "getUTCHours",
+      utc_hours: "getUTCHours",
+      utcMilliseconds: "getUTCMilliseconds",
+      UTCMilliseconds: "getUTCMilliseconds",
+      utc_milliseconds: "getUTCMilliseconds",
+      utcMinutes: "getUTCMinutes",
+      UTCMinutes: "getUTCMinutes",
+      utc_minutes: "getUTCMinutes",
+      utcMonth: "getUTCMonth",
+      UTCMonth: "getUTCMonth",
+      utc_month: "getUTCMonth",
+      utcSeconds: "getUTCSeconds",
+      UTCSeconds: "getUTCSeconds",
+      utc_seconds: "getUTCSeconds"
+    };
+
+    store(specs).forEach(function (method, field) {
+      it(field, function () {
+        expect(stored(field)).toBe(date[method]());
+      });
     });
   });
 
   describe("implements dumping", function () {
-    var date = store(new Date());
+    var date = new Date();
+    var stored = store(date);
 
     it("with dump method", function () {
-      expect(date.dump()).toEqual(date());
+      expect(stored.dump()).toEqual(date);
     });
 
     it("with json method", function () {
-      expect(date.json()).toEqual(JSON.stringify(date()));
+      expect(stored.json()).toEqual(JSON.stringify(date));
     });
   });
 });
